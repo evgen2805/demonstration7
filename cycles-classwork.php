edbfda69bafe91a3630fdfdaf79f6c47c83ef398
evgen2805 <evgen3344@mail.ru>
2016-06-29 20:24:09 +0300

    Create cycles-classwork.php

---
+++ cycles-classwork.php
@@ -0,0 +1,19 @@
+<?php
+/*
+1. создать массив, в котором есть перечень автомобильных брендов
+2. вывести все бренды через запятую на экран
+*/
+
+
+  $AvtoSalon = array(
+         'brands' => array('Ferarri','mersedes','audi';
+    
+             echo print_r(array_keys($AvtoSalon));
+
+
+
+
+
+
+
+?>
