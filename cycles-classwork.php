1d621dc369892ea7960b970d89db8bd8f8fb6637
evgen2805 <evgen3344@mail.ru>
2016-06-29 20:55:07 +0300

    Update cycles-classwork.php

--- cycles-classwork.php
+++ cycles-classwork.php
@@ -4,31 +4,20 @@
 2. вывести все бренды через запятую на экран
 */
 
-
-  $AvtoSalon = array(
-         'brands' => array('Ferarri','mersedes','audi';
-    
-             echo print_r(array_keys($AvtoSalon));
-for ($i = 1; $i <=3; $i++) {
-    echo $i;
-}
-
-
-
-
-?>
 <?php 
 
-$auto = array ( 'HUNDAI', 'KIA', 'DAEWOO',);
+$auto = array ( 'lotus', 'ferrari', 'Audi',);
 $arrayCount = count($auto);
-for ($i=2; $i < $arrayCount; ++$i ) {
-	echo $auto[$i];
-    echo ",";
+
+
+for($i = $arrayCount - 1; $i >= 0; $i--) {
+    echo $auto[$i] . ', ';
 }
-
-
 
 
 
 
  ?>
+
+
+ 
