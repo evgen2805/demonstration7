c36ff9e5aa8d72bc8584352ce91562988fa1e96e
evgen2805 <evgen3344@mail.ru>
2016-06-29 20:38:22 +0300

    Update cycles-classwork.php

--- cycles-classwork.php
+++ cycles-classwork.php
@@ -9,11 +9,26 @@
          'brands' => array('Ferarri','mersedes','audi';
     
              echo print_r(array_keys($AvtoSalon));
-
-
-
+for ($i = 1; $i <=3; $i++) {
+    echo $i;
+}
 
 
 
 
 ?>
+<?php 
+
+$auto = array ( 'HUNDAI', 'KIA', 'DAEWOO',);
+$arrayCount = count($auto);
+for ($i=2; $i < $arrayCount; ++$i ) {
+	echo $auto[$i];
+    echo ",";
+}
+
+
+
+
+
+
+ ?>
