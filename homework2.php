7a8e38b80d6f83b31048a9dd0930cb67709dac64
evgen2805 <evgen3344@mail.ru>
2016-07-02 01:32:08 +0300

    Update homework2.php

--- homework2.php
+++ homework2.php
@@ -3,3 +3,51 @@
 3. нужно вывести данные в таком формате
 */
 
+<?php
+$carCenter = array(
+    'brands' => array(
+        'bmw' => array(
+            'models' => array(
+                'm5' => array(
+                    'years' => array(
+                        1999,
+                        2000
+                    )
+                ),
+                'm6' => array(
+                    'years' => array(
+                        2001,
+                        2002
+                    )
+                )
+            )
+        )
+    )
+);
+$carCenter['brands']['bmw']['models']['m6']['years'];
+$carCenter = array(
+    'bmw' => array(
+        'm5' => array(
+            1999,
+            2001
+        ),
+        'm6' => array(
+            2001,
+            2002
+        )
+    ),
+    'toyota' => array(
+        'camry' => array(1999)
+    )
+);
+// 1. использовать for и foreach
+// 2. использовать два вида массива
+// 3. нужно вывести данные в таком формате:
+Model: bmw
+Brands: m5, m6
+years: 1999, 2001
+-----------------
+Model: toyota
+Brands: camry
+years: 1999
+
