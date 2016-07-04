1668524976ebb67000d3bcebaad262408a6ee8f2
evgen2805 <evgen3344@mail.ru>
2016-07-04 21:22:39 +0300

    Create classwork7.php

---
+++ classwork7.php
@@ -0,0 +1,14 @@
+<?php
+$Shop  = array( 
+    'Rost', 
+    'Klas', 
+    'Silpo',
+    'billa');
+
+  $arrayCount = count($Shop);
+  
+  for($i = $arrayCount ; $i >= 0; $i++) {
+    echo $shop [$i] . ', ';
+}
+
+?>
