2d99f4188cf1bca7ddba4e3bfe3f041ad127f07b
evgen2805 <evgen3344@mail.ru>
2016-07-06 10:47:51 +0300

    Create Praktika1.php

---
+++ Praktika1.php
@@ -0,0 +1,23 @@
+<?php
+$posts = [
+ 228445 =>[
+      'title' => 'Some title #1',
+      'text' => 'some text #1'
+  ]
+  163685 =>[
+      'title' => 'Some title #2',
+      'text' => 'some text #2'
+  ]
+];
+foreach ($posts as $postId => $postData){
+var_dump($postData);
+echo "<br/>";
+}
+
+
+
+
+
+
+
+?>
